0e969f562a0a2304365d16c08ee1555ca7b37115
Rajat <rs20021023@gmail.com>
2022-01-20 20:41:30 +0530

    populate books from array and can add new books

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-const titleInput = document.querySelector(".title-input")
-const authorInput = document.querySelector(".author-input")
-const pageInput = document.querySelector(".page-input")
-const readInput = document.querySelector(".read-input")
-const addBtn = document.querySelector(".add-book")
-
-
-const handleAddBtn = () =>{
-    console.log(titleInput.value,authorInput.value,pageInput.value)
-}
-
-
-addBtn.addEventListener("click",handleAddBtn)
-
-
+const titleInput = document.querySelector(".title-input");
+const authorInput = document.querySelector(".author-input");
+const pageInput = document.querySelector(".page-input");
+const readInput = document.querySelector(".read-input");
+const addBtn = document.querySelector(".add-book");
+const cardGroup = document.querySelector(".card-group");
+const deleteBtn = document.querySelector(".delete-btn");
 
 let library = [];
-function Books(name, author, pages, isRead) {
-  this.name = name;
+function Books(title, author, pages, isRead) {
+  this.title = title;
   this.author = author;
   this.pages = pages;
   this.isRead = isRead;
@@ -26,15 +18,56 @@ function addBookToLibrary(book) {
   library.push(book);
 }
 
-let b1 = new Books("rajat", "rajat", 234, "yes");
-let b2 = new Books("beb", "rajat", 234, "yes");
-let b3 = new Books("rajat", "rajat", 234, "yes");
-let b4 = new Books("rajat", "rajat", 234, "yes");
+let b1 = new Books("rajat", "Sharma", 234, "yes");
+let b2 = new Books("manu", "rasdfsdfjat", 234, "yes");
+let b3 = new Books("anuj", "rasdfsjat", 234, "yes");
+let b4 = new Books("kartik", "rasdfdfjat", 234, "yes");
 addBookToLibrary(b1);
 addBookToLibrary(b2);
 addBookToLibrary(b3);
 addBookToLibrary(b4);
 
-// library.forEach((item) =>{
-//     console.log(item["name"])
-// })
+const makeNewCard = (newTitle, newAuthor, newPages, isRead) => {
+  const card = document.createElement("div");
+  const cardBody = document.createElement("div");
+  const bookTitle = document.createElement("h2");
+  const bookAuthor = document.createElement("h3");
+  const pages = document.createElement("p");
+  const newDeleteBtn = document.createElement("button");
+
+  card.classList.add("card");
+  cardBody.classList.add("card-body");
+  bookTitle.classList.add("book-title");
+  bookAuthor.classList.add("book-author");
+  pages.classList.add("pages");
+  newDeleteBtn.classList.add("delete-btn");
+
+  bookTitle.textContent = newTitle;
+  bookAuthor.textContent = newAuthor;
+  pages.textContent = `Pages: ${newPages}`;
+  newDeleteBtn.textContent = "Delete";
+
+  cardGroup.appendChild(card);
+  card.appendChild(cardBody);
+  cardBody.appendChild(bookTitle);
+  cardBody.appendChild(bookAuthor);
+  cardBody.appendChild(pages);
+  cardBody.appendChild(newDeleteBtn);
+};
+
+const handleAddBtn = (e) => {
+  e.preventDefault();
+  let newBook = new Books(titleInput.value, authorInput.value, pageInput.value);
+  library.push(newBook);
+  makeNewCard(newBook["title"], newBook["author"], newBook["pages"]);
+};
+
+const display = () => {
+  library.forEach((item) => {
+    makeNewCard(item["title"], item["author"], item["pages"]);
+  });
+};
+
+display();
+
+addBtn.addEventListener("click", handleAddBtn);
